37bbdd83e90c9759b01a58c3d605277600ddca32
david-ae <davidedet47@gmail.com>
2024-02-16 07:36:44 +0100

    Added Components

---
+++ src/app/components/negotiation/negotiation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-negotiation',
+  standalone: true,
+  imports: [],
+  templateUrl: './negotiation.component.html',
+  styleUrl: './negotiation.component.css'
+})
+export class NegotiationComponent {
+
+}
