63d1df8372e289f41433dbd6796ec1fc8688e12d
david-ae <davidedet47@gmail.com>
2024-02-25 22:48:06 +0100

    tabs modification

--- src/app/components/item-management/item-management.component.ts
+++ src/app/components/item-management/item-management.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-item-management',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './item-management.component.html',
   styleUrl: './item-management.component.css'
 })
 export class ItemManagementComponent {
-
+  logChange(index: unknown) {
+    console.log(index);
+  }
 }
